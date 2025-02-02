10eeb49dd79931f755c3700e338b40634c94f9be
Tsvetan <tsvetanml@gmail.com>
2025-02-02 16:24:31 +0100

    Add animated background component using tsparticles for dynamic visuals

---
+++ app/components/animatedBackground/index.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { Container, ISourceOptions, MoveDirection, OutMode } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useMemo, useState } from "react";
+
+const AnimatedBackground = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "transparent",
+        },
+      },
+      fpsLimit: 60,
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: "attract",
+          },
+          resize: {
+            enable: true, // Corrección: 'resize' debe ser un objeto con 'enable'
+          },
+        },
+        modes: {
+          attract: {
+            distance: 150,
+            duration: 0.3,
+            factor: 2,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: ["#96b5f2", "#4b9def", "#1e3a8a"],
+        },
+        links: {
+          color: "#96b5f2",
+          distance: 120,
+          enable: true,
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: true,
+          speed: 1.5,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 50,
+        },
+        opacity: {
+          value: { min: 0.3, max: 0.7 },
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 4 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+        style={{ position: "absolute", inset: 0, zIndex: 0 }}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default AnimatedBackground;
