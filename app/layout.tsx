f79153016eb640c1a65f84c7fe79f5f6b8fbdb8a
Tsvetan <tsvetanml@gmail.com>
2025-02-02 16:25:04 +0100

    Integrate animated background and dropdown menu components into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import AnimatedBackground from "./components/animatedBackground";
+import DropdownMenu from "./components/dropdownMenu";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,14 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={"antialiased bg-white "}
-      >
-        <div className="absolute h-screen w-full bg-[radial-gradient(#96b5f2_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]">
-
+      <body className="antialiased bg-white relative overflow-hidden">
+        <AnimatedBackground />
+        <div className="relative z-10">
+          <DropdownMenu />
           {children}
-
         </div>
+        {/* </div> */}
       </body>
     </html>
   );
